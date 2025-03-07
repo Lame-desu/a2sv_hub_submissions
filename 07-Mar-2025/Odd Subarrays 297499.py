8ed2ab25e0eaa5086e28c073fafa64cc65a6337f
Lamesgin Desalegn <143926337+Lame-desu@users.noreply.github.com>
2025-03-07 22:22:07 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 07-Mar-2025/Odd Subarrays 297499.py
@@ -0,0 +1,36 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+import sys
+import itertools
+
+def input():
+    return sys.stdin.readline().strip()
+
+def read_ints():
+    return list(map(int, input().split()))
+
+def read_int():
+    return int(input())
+
+def read_matrix(rows):
+    return [read_ints() for _ in range(rows)]
+
+def run_tests(solve):
+    t = read_int()
+    for _ in range(t):
+        solve()
+
+def solve():
+    n = read_int()
+    array = read_ints()
+    count = i = 0
+    while i < len(array) - 1:
+        if array[i] > array[i+1]:
+            count += 1
+            i+=1
+        i+=1
+    print(count)
+
+if __name__ == '__main__':
+    # solve()
+    run_tests(solve)
